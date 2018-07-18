94e15154d080cb6882b52d0544eecae0886eed9b
Noelle Foster <uufarmgirl@gmail.com>
2018-07-18 17:34:48 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -17,6 +17,15 @@ function divide(a, b){
 function inc(a){
   return a+=1
 }
+
 function dec(a){
   return a-=1
+}
+
+function makeInt(n){
+  return parseInt(n, 10)
+}
+
+function preserveDecimal(n){
+  return parseFloat(n)
 }
